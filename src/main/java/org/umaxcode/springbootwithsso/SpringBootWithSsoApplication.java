8c32bf0ad5a2036fab678eb8a1787613cd259347
umaxcode <maxwellodoom1729@gmail.com>
2025-04-15 12:24:42 +0000

    chore: initial project setup

---
+++ src/main/java/org/umaxcode/springbootwithsso/SpringBootWithSsoApplication.java
@@ -0,0 +1,13 @@
+package org.umaxcode.springbootwithsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/umaxcode/springbootwithsso/SpringBootWithSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umaxcode.springbootwithsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
